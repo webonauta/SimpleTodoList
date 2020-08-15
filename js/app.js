4d5c59c4553032a581c3e1790f12fcdb2e288d3b
Alberto Gabino <35282567+webonauta@users.noreply.github.com>
2020-08-14 22:55:27 -0500

    Update app.js
    Agregando variables, escuhas y agregando funcion para agregar tareas a la lista

---
+++ js/app.js
@@ -0,0 +1,22 @@
+const listaTareas = document.getElementById('lista-tareas')
+
+addEventListener()
+
+function addEventListener(){
+    document.querySelector('#formulario').addEventListener('submit', agregarTarea)
+    listaTareas.addEventListener('click', borrarTarea)
+}
+
+function agregarTarea(e){
+    e.preventDefault()
+
+    const tarea = document.getElementById('tarea').value
+    const botonBorrar = document.createElement('a')
+    botonBorrar.classList = 'borrar-tarea'
+    botonBorrar.innerText = 'X'
+
+    const li = document.createElement('li')
+    li.innerText = tarea
+    li.appendChild(botonBorrar)
+    listaTareas.appendChild(li)
+}
